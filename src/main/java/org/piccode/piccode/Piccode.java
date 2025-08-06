cb7831dd6f9806f2dfe4dd77c457d32da5248abd
hexaredecimal <mfanakagama@gmail.com>
2025-08-07 00:57:29 +0200

    Piccode: register native functions at the start

--- src/main/java/org/piccode/piccode/Piccode.java
+++ src/main/java/org/piccode/piccode/Piccode.java
@@ -2,6 +2,7 @@ package org.piccode.piccode;
 
 import org.editor.AccessFrame;
 import org.editor.EditorWindow;
+import org.editor.nativemods.PiccodeGfxModule;
 import org.piccode.backend.Compiler;
 import org.piccode.piccodescript.ErrorAsciiKind;
 
@@ -15,7 +16,11 @@ public class Piccode {
 		Compiler.exitOnError = false;
 		Compiler.errorKind = ErrorAsciiKind.EMACS_COMP_STYLE;
 		Compiler.out = AccessFrame.AccessFrameOutputStream.out;
-
+		initializeNativeAppModules();
 		EditorWindow.the();
+	}
+
+	private static void initializeNativeAppModules() {
+		Compiler.addNativeFunctions(PiccodeGfxModule::addFunctions);
 	}
 }
